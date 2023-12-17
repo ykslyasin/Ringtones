1d965d6f4530b66f0961d46acdf2993ac6939d76
Yasin YÜKSEL <yksl.yasin@yahoo.com>
2023-12-17 18:59:57 +0300

    2

--- src/main/java/com/example/Ringtones/entities/concretes/ShoppingCart.java
+++ src/main/java/com/example/Ringtones/entities/concretes/ShoppingCart.java
@@ -2,11 +2,16 @@ package com.example.Ringtones.entities.concretes;
 
 import java.util.List;
 
+import com.example.Ringtones.core.entities.User;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,7 +28,11 @@ public class ShoppingCart {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
-	private int userId;
+	private int shoppingCartId;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "fk_usr_id")
+	private User user;
 	
 	@NotNull
 	@NotBlank
