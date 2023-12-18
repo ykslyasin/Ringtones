3d0e5c0a98fdfd6b619f884d8c5312e1b0cd722c
Yasin YÜKSEL <yksl.yasin@yahoo.com>
2023-12-18 16:17:05 +0300

    Update UserController.java

--- src/main/java/com/example/Ringtones/api/controllers/UserController.java
+++ src/main/java/com/example/Ringtones/api/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping(value="/api/users")
+@CrossOrigin
 public class UserController {
 
 	private UserService userService;
