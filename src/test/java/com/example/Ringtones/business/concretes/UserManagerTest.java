8c7e1b7db254362f59bb6869ba3ffc32d9ae59cd
Yasin YÜKSEL <yksl.yasin@yahoo.com>
2023-12-30 18:17:51 +0300

    1

--- src/test/java/com/example/Ringtones/business/concretes/UserManagerTest.java
+++ src/test/java/com/example/Ringtones/business/concretes/UserManagerTest.java
@@ -2,16 +2,17 @@ package com.example.Ringtones.business.concretes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.anyString;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -22,7 +23,6 @@ import com.example.Ringtones.core.utilities.results.DataResult;
 import com.example.Ringtones.core.utilities.results.ErrorResult;
 import com.example.Ringtones.core.utilities.results.Result;
 import com.example.Ringtones.core.utilities.results.SuccessResult;
-import com.example.Ringtones.entities.concretes.Ringtone;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
